4dcef9f2ebb05429346cb7b6003160b7a032d138
Thrang <thrangrushecda1@gmail.com>
2019-08-13 21:51:47 +0700

    - Test Create a window

--- Render3dProject/Render3dProject/Render3dProject.cpp
+++ Render3dProject/Render3dProject/Render3dProject.cpp
@@ -2,11 +2,62 @@
 //
 
 #include "pch.h"
+#include <Windows.h>
 #include <iostream>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 int main()
 {
     std::cout << "Hello World!\n"; 
+
+	const wchar_t CLASS_NAME[] = L"Sample Window Class";
+
+	WNDCLASS wc = { };
+
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = NULL;
+	wc.lpszClassName = CLASS_NAME;
+
+	RegisterClass(&wc);
+
+	HWND hwnd = CreateWindowEx(0,
+		CLASS_NAME,
+		L"Sample Window Name",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL,
+		NULL,
+		NULL,
+		NULL);
+
+	if (hwnd != NULL)
+	{
+		std::cout << "ShowWindow\n";
+		ShowWindow(hwnd, SW_SHOWDEFAULT);
+	}
+
+	MSG msg = { };
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			return 0;
+
+		default:
+			std::cout << '.';
+			return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
