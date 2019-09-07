2f984998f120a1955fb3b5c78fa47f14630168f0
Thrang <thrangrushecda1@gmail.com>
2019-09-07 15:03:22 +0700

    Close window when pressing OK button

--- Render3dProject/Render3dProject/Render3dProject.cpp
+++ Render3dProject/Render3dProject/Render3dProject.cpp
@@ -7,10 +7,11 @@
 #include <WinUser.h>
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+HWND hwnd;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	std::cout << "Hello World!\n";
 
 	const wchar_t CLASS_NAME[] = L"Sample Window Class";
 
@@ -22,32 +23,46 @@ int main()
 
 	RegisterClass(&wc);
 
-	HWND hwnd = CreateWindowEx(0,
-		CLASS_NAME,
-		L"Sample Window Name",
-		WS_OVERLAPPEDWINDOW,
-		600, 337, 400, 225,
-		NULL,
-		NULL,
-		NULL,
-		NULL);
+	hwnd = CreateWindowEx(0,
+							   CLASS_NAME,
+							   L"Sample Window Name",
+							   WS_OVERLAPPEDWINDOW,
+							   600, 337, 400, 225,
+							   NULL,
+							   NULL,
+							   NULL,
+							   NULL);
 
-	HWND hwndButton = CreateWindow(
-		L"BUTTON",  // Predefined class; Unicode assumed 
-		L"OK",      // Button text 
-		WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles 
-		150,         // x position 
-		120,         // y position 
-		100,        // Button width
-		30,        // Button height
-		hwnd,     // Parent window
-		NULL,       // No menu.
-		(HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
-		NULL);      // Pointer not needed.
+	HWND hwndButton = CreateWindowEx(0,
+									 L"BUTTON",
+									 L"OK",
+									 WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+									 150,
+									 120,
+									 100,
+									 30,
+									 hwnd,
+									 (HMENU)1001,
+									 (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
+									 NULL);
+
+	HWND hwndCheckbox = CreateWindowEx(0,
+									   L"BUTTON",
+									   L"OpenGL",
+									   WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_AUTORADIOBUTTON,
+									   50,
+									   50,
+									   100,
+									   30,
+									   hwnd,
+									   NULL,
+									   //NULL,
+									   (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
+									   NULL);
 
 	if (hwnd != NULL)
 	{
-		std::cout << "ShowWindow\n";
+		std::cout << "Show Window\n";
 		ShowWindow(hwnd, SW_SHOWDEFAULT);
 	}
 
@@ -63,15 +78,40 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	switch (uMsg)
 	{
+		case WM_PAINT:
+		{
+			PAINTSTRUCT ps;
+			HDC hdc = BeginPaint(hwnd, &ps);
+
+			FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW));
+
+			EndPaint(hwnd, &ps);
+			break;
+		}
+
+		case WM_COMMAND:
+		{
+			if (LOWORD(wParam) == 1001)
+			{
+				printf("Button clicked\n");
+				DestroyWindow(hwnd);
+			}
+			break;
+		}
+
 		case WM_DESTROY:
+		{
 			PostQuitMessage(0);
-			return 0;
+			break;
+		}
 
 		default:
+		{
 			POINT cursorPosition;
 			GetCursorPos(&cursorPosition);
-			std::cout << cursorPosition.x << " " << cursorPosition.y << "\n";
+			//std::cout << cursorPosition.x << " " << cursorPosition.y << "\n";
 			return DefWindowProc(hwnd, uMsg, wParam, lParam);
+		}
 	}
 	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
