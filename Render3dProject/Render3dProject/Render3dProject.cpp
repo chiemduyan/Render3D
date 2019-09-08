a2b527c2db61ea8f0f6fd02029e9b9fd04e76d32
Thrang <thrangrushecda1@gmail.com>
2019-09-08 15:12:22 +0700

    Show OpenGL window

--- Render3dProject/Render3dProject/Render3dProject.cpp
+++ Render3dProject/Render3dProject/Render3dProject.cpp
@@ -2,12 +2,22 @@
 //
 
 #include "pch.h"
-#include <Windows.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
+#include <Windows.h>
 #include <WinUser.h>
+
+#include <glm/glm.hpp>
+using namespace glm;
+
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 HWND hwnd;
+GLFWwindow* window;
+
+void InitializeOpenGL();
+void CreateOpenGLWindow();
 
 int main()
 {
@@ -72,6 +82,23 @@ int main()
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
+
+	// Ensure we can capture the escape key being pressed below
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	do {
+		// Clear the screen. It's not mentioned before Tutorial 02, but it can cause flickering, so it's there nonetheless.
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Draw nothing, see you in tutorial 2 !
+
+		// Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	} // Check if the ESC key was pressed or the window was closed
+	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+		   glfwWindowShouldClose(window) == 0);
 }
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -93,6 +120,9 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		{
 			if (LOWORD(wParam) == 1001)
 			{
+				InitializeOpenGL();
+				CreateOpenGLWindow();
+
 				printf("Button clicked\n");
 				DestroyWindow(hwnd);
 			}
@@ -114,6 +144,41 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		}
 	}
 	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+void InitializeOpenGL()
+{
+	// Initialise GLFW
+	glewExperimental = true; // Needed for core profile
+	if (!glfwInit())
+	{
+		fprintf(stderr, "Failed to initialize GLFW\n");
+		return;
+	}
+}
+
+void CreateOpenGLWindow()
+{
+	glfwWindowHint(GLFW_SAMPLES, 4); // 4x antialiasing
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL 
+
+	// Open a window and create its OpenGL context
+
+	window = glfwCreateWindow(1024, 768, "Tutorial 01", NULL, NULL);
+	if (window == NULL) {
+		fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+		glfwTerminate();
+		return;
+	}
+	glfwMakeContextCurrent(window); // Initialize GLEW
+	glewExperimental = true; // Needed in core profile
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		return;
+	}
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
